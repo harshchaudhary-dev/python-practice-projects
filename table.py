a651d6ae4a56672bea0379b24b6fd7902112c8ea
Harsh Chaudhary <harshchaudhary.codes@gmail.com>
2026-07-05 09:09:44 +0530

    table.py
    Add multiplication table program in Python

---
+++ table.py
@@ -0,0 +1,14 @@
+print ("Smart table")
+num = int(input("Enter a number to print its table: "))
+print ("Here is your table")
+for i in range(1 , 11) :
+    result = num * i
+    print(num, "x", i, "=", result)
+    
+    
+# for practice
+print("Table")
+num=int(input("Enter the number: "))
+for i in range(1, 11) :
+    result = num * i
+    print(num, "×", i, "=", result)
